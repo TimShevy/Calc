9a519426ad1fa86329f2c2e05689568a9baf64e4
TimShevy <126278688+TimShevy@users.noreply.github.com>
2023-02-25 15:38:50 +0500

    Добавил еще одну конструкцию ifElse
    Добавил вопросы об имени и возрасте с вариантами ответов!

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 let a = +prompt('Введите первое число:')
 let action = prompt(`Какое действие Вы хотите выполнить:
 "sum" - сумма двух чисел?
-"average" - среднее арифметическое?
+"ave" - среднее арифметическое?
 "     +     " ?
 "     -     " ?
 "     *     " ?
-"     /     " ?`)
+"     /     " ?
+"     %     " ?
+"     **    " ?`)
 
 let b = +prompt('Введите второе число:')
 
@@ -32,15 +34,46 @@ if (action == "+") {
                     answer = a + b
                     message = (`Сумма двух чисел равна: ${answer}`)
                 } else {
-                    if (action == "average") {
+                    if (action == "ave") {
                         answer = (a + b) / 2
                         message = (`Среднее арифметическое двух чисел равно: ${answer}`)
                     } else {
-                        message = (`Ошибка ввода, внимательно читайте инструкцию!`)
+                        if (action == "**") {
+                            answer = a ** b
+                            message = (`${a} в степени ${b} равно: ${answer}`)
+                       } else {
+                            if (action == "%") {
+                                answer = a / 100 * b
+                                message = (`${b} процентов от ${a} равно: ${answer}`)
+                            } else {
+                                message = (`Ошибка ввода, внимательно читайте инструкцию!`)
+                            }
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+alert(message)
+
+let myAge = 33
+let name = prompt('Введите имя:')
+let age = +prompt('Введите возраст:')
+let isMale = confirm('Вы мужчина?:')
+
+if (age > myAge) {
+    message = `Здравствуйте, ${name}!`
+} else {
+    if (isMale == true) {
+        message = `Привет, ${name}!`
+    } else {
+       if (18 <= age) {
+            message = `Привет, ${name}! Давай познакомимся! Вот мой номер телефона: +998-90-097-xx-xx`
+       } else {
+            message = `Здравствуйте, ${name}!`
+       }
     }
 }
 
