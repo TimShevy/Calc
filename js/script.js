73b8f6db3210d85121ea7b2d8a6964f1f934fddd
TimShevy <126278688+TimShevy@users.noreply.github.com>
2023-03-12 19:20:15 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+function addSymbol() {
+    let btn = event.target
+    let sym = btn.innerHTML
+    let userText = document.getElementById('userText')
+    userText.innerHTML += sym 
+}
+
+function clear() {
+    let btnClear = event.target
+    let userText = document.getElementById('userText')
+    btnClear = userText.innerHTML.slice(0, -1) //replace(/.$/, '')
+    userText.innerHTML = btnClear
+}
+
+let keys = document.querySelectorAll('.keyboard .key')
+let keyClear = document.querySelectorAll('.keyboard .clear')
+let userText = document.getElementById('userText')
+
+let isCapsLockOn = false;
+document.getElementById('capslock').addEventListener('click', function() {
+    isCapsLockOn = !isCapsLockOn;
+    let keys = document.querySelectorAll('.key');
+    keys.forEach(function(key) {
+        
+        let keyText = key.textContent;
+        if (isCapsLockOn) {
+            key.textContent = keyText.toUpperCase();
+        } else {
+            key.textContent = keyText.toLowerCase();
+        }
+    });
+});
+
+keys.forEach( btn => {
+    btn.addEventListener('click', addSymbol)
+})
+
+keyClear.forEach( btnClear => {
+    btnClear.addEventListener('click', clear)
+})
+
+
+
 // // let a = +prompt('Введите первое число:')
 // // let action = prompt(`Какое действие Вы хотите выполнить:
 // // "sum" - сумма двух чисел?
@@ -138,43 +181,3 @@
 
 // alert(`${name} ${age}`)
 
-function addSymbol() {
-    let btn = event.target
-    let sym = btn.innerHTML
-    let userText = document.getElementById('userText')
-    userText.innerHTML += sym 
-}
-
-function clear() {
-    let btnClear = event.target
-    let userText = document.getElementById('userText')
-    btnClear = userText.innerHTML.slice(0, -1) //replace(/.$/, '')
-    userText.innerHTML = btnClear
-}
-
-let keys = document.querySelectorAll('.keyboard .key')
-let keyClear = document.querySelectorAll('.keyboard .clear')
-let userText = document.getElementById('userText')
-
-let isCapsLockOn = false;
-document.getElementById('capslock').addEventListener('click', function() {
-    isCapsLockOn = !isCapsLockOn;
-    let keys = document.querySelectorAll('.key');
-    keys.forEach(function(key) {
-        
-        let keyText = key.textContent;
-        if (isCapsLockOn) {
-            key.textContent = keyText.toUpperCase();
-        } else {
-            key.textContent = keyText.toLowerCase();
-        }
-    });
-});
-
-keys.forEach( btn => {
-    btn.addEventListener('click', addSymbol)
-})
-
-keyClear.forEach( btnClear => {
-    btnClear.addEventListener('click', clear)
-})
